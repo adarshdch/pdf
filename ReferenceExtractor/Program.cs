ee4ad931512891af4beb2651c071168d9f09ce5a
Adarsh Kumar <adarshdch@gmail.com>
2015-06-16 13:00:22 +0530

    update

--- ReferenceExtractor/Program.cs
+++ ReferenceExtractor/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using iTextSharp.text.pdf;
 using iTextSharp.text.pdf.parser;
@@ -12,14 +13,15 @@ namespace ReferenceExtractor
 
 	class Converter
 	{
-		public static string Convert()
+		const string strPath = @"C:\D\EBSCO\pdf\data\samples\10.1177_0003319714535238.pdf";
+		
+		public static string ConvertBasic()
 		{
-			const string strPath = @"C:\D\EBSCO\pdf\data\samples\10.1177_0003319714535238.pdf";
 			using (var reader = new PdfReader(strPath))
 			{
 				var text = new StringBuilder();
 
-				for (int i = 1; i <= reader.NumberOfPages; i++)
+				for (int i = reader.NumberOfPages-5; i <= reader.NumberOfPages; i++)
 				{
 					text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
 				}
@@ -29,12 +31,41 @@ namespace ReferenceExtractor
 				return text.ToString();
 			}
 		}
+
+		public static string ConvertAdvanced()
+		{
+			ITextExtractionStrategy its = new iTextSharp.text.pdf.parser.LocationTextExtractionStrategy();
+
+			using (PdfReader reader = new PdfReader(strPath))
+			{
+				StringBuilder text = new StringBuilder();
+
+				for (int i = 1; i <= reader.NumberOfPages; i++)
+				{
+					string thePage = PdfTextExtractor.GetTextFromPage(reader, i, its);
+					string[] theLines = thePage.Split('\n');
+					foreach (var theLine in theLines)
+					{
+						text.AppendLine(theLine);
+					}
+				}
+				return text.ToString();
+			}
+		}
 	}
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.Write(Converter.Convert());
+			//var aContent = Converter.ConvertBasic();
+			var aContent = File.ReadAllText(@"D:\test.txt");
+
+			var aReferenceText = ReferencesExtractor.GetAllReferencesText(aContent);
+			var aReferencesText = ReferenceExtractor.ReferencesExtractor.GetAllReferences(aReferenceText).ToList();
+
+			aReferencesText.ForEach(Console.WriteLine);
+			
+
 			Console.ReadKey();
 		}
 
---
+++ ReferenceExtractor/ReferencesExtractor.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReferenceExtractor
+{
+	class ReferencesExtractor
+	{
+		public static string GetAllReferencesText(string aContent)
+		{
+			var aIndex = aContent.LastIndexOf("\nReferences", StringComparison.InvariantCultureIgnoreCase);
+			return aContent.Substring(aIndex);
+		}
+
+		public static IList<string> GetAllReferences(string aContent)
+		{
+			IList<string> aReferences = new List<string>();
+
+			int aCurrentRefereneIndex = 1;
+
+			while (string.IsNullOrWhiteSpace(aContent) == false)
+			{
+				var aReferenceStartIndex = aContent.IndexOf(string.Format("\n{0}.", aCurrentRefereneIndex),
+					StringComparison.InvariantCultureIgnoreCase);
+				var aReferenceEndIndex = aContent.IndexOf(string.Format("\n{0}.", aCurrentRefereneIndex + 1),
+					StringComparison.InvariantCultureIgnoreCase);
+				if (aReferenceStartIndex < 0 || aReferenceEndIndex < 0)
+					break;
+				var aReferenceText = aContent.Substring(aReferenceStartIndex, aReferenceEndIndex - aReferenceStartIndex);
+				aContent = aContent.Substring(aReferenceEndIndex);
+				aCurrentRefereneIndex ++;
+
+				aReferences.Add(aReferenceText);
+			}
+			return aReferences;
+		}
+
+	}
+}
